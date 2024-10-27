fd5e0c11ad75c58abfb35e4515839251dc17313a
luongp134 <80845739+luongp134@users.noreply.github.com>
2024-10-27 10:57:31 -0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -67,36 +67,88 @@ def extract_next_links(url, resp):
     return hyperlinks
 
 def is_valid(url):
-    # Decide whether to crawl this url or not. 
+    # Decide whether to crawl this url or not.
     # If you decide to crawl it, return True; otherwise return False.
     # There are already some conditions that return False.
+    global visit_count
+
     try:
-        parsed = urlparse(url)
-        if parsed.scheme not in set(["http", "https"]):
+        url_without_fragment = urldefrag(url).url  # need for defragging of url
+        parsed = urlparse(url_without_fragment)
+        valid_domains = [
+            ".ics.uci.edu",
+            "cs.uci.edu",
+            ".informatics.uci.edu",
+            ".stat.uci.edu",
+        ]
+
+        # Preliminary checks for URL scheme, hostname, and query characters
+        if (
+            not parsed.scheme in {"http", "https"}
+            or parsed.hostname is None
+            or parsed.netloc is None
+            or "?" in url
+            or "&" in url
+        ):
             return False
-        if re.search(r"/\d{4}/\d{2}/\d{2}/", parsed.path): #calendar/dates
+
+        if not any(domain in parsed.hostname for domain in valid_domains):
             return False
-        if re.search(r"/(search|login|logout|api|admin|raw|static)/", parsed.path.lower()): #low value path
+
+        # Exclude URLs that match the following criteria:
+        # 1. Include a valid domain from the `valid_domains` list.
+        # 2. Do not include certain file extensions or paths indicating non-content URLs.
+        # 3. Patterns to exclude low-value paths, infinite pages
+        if (
+            re.search(
+                r"/(search|login|logout|api|admin|raw|static)/", parsed.path.lower()
+            ) # low-value path
+            or re.search(r"/(page|p)/?\d+", parsed.path.lower())  # infinite pages
+            or re.search(r"(sessionid|sid|session)=[\w\d]{32}", parsed.query) # sessionID or random long string
+            or re.match(
+                r".*\.(css|js|bmp|gif|jpe?g|ico|png|tiff?|mid|mp2|mp3|mp4"
+                r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx"
+                r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso|epub|dll|cnf|tgz|sha1"
+                r"|thmx|mso|arff|rtf|jar|csv|rm|smil|wmv|swf|wma|zip|rar|gz)$",
+                parsed.path.lower(), # end with a specific non-useful set of file extensions
+            )
+            or re.search(
+            r"/\d{4}/\d{2}/\d{2}/|/\d{4}-\d{2}-\d{2}/|\/(19|20)[0-9]{2}/|\/(19|20)[0-9]{2}$|\/(19|20)[0-9]{2}-[0-9]{1,2}",
+            parsed.path.lower(), # date-like patterns
+        )):
             return False
-        if re.search(r"/(page|p)/?\d+", parsed.path.lower()): #infinite pages
-            return False
-        if re.search(r"(sessionid|sid|session)=[\w\d]{32}", parsed.query): #sessionID / random long str.
-            return False
-        
-        return not re.match(
-            r".*\.(css|js|bmp|gif|jpe?g|ico"
-            + r"|png|tiff?|mid|mp2|mp3|mp4"
-            + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
-            + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
-            + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
-            + r"|epub|dll|cnf|tgz|sha1"
-            + r"|thmx|mso|arff|rtf|jar|csv"
-            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
+        return True
+
+    except (ValueError, TypeError) as e:
+        print(f"Error for URL {url}: {e}")
+        return False
 
 
-    except TypeError:
-        print ("TypeError for ", parsed)
-        raise
+def get_output():
+    # returns the answer to all four problems
+    # TODO: need other helper function for completion
+    holder = ""
+
+    # Question 1: Number of unique pages found
+    holder += f"1. Number of unique pages found: <<helper function>>"
+
+    # Question 2: Longest page in terms of number of words
+    holder += (
+        f"2. Longest page: <<helper function>> with <<helper function>> words total\n\n"
+    )
+
+    # Question 3: 50 most common words
+    holder += (
+        "3. 50 most common words in order of most frequent to least frequent are:\n"
+    )
+
+    # Question 4: Subdomains found and total number of subdomains
+
+    # Write holder to a text file
+    with open("result.txt", "w") as file:
+        file.write(holder)
+
+    print("Result has been written to result.txt file.")
 
 """
     tokenize: 
